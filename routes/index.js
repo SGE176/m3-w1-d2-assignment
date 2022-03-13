f802fdd3cb838b2de81b0495c090b3ce29506715
SGE176 <s.ge.176@westcliff.edu>
2022-03-12 21:38:42 -0600

    w2 lab 1st commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
+const path = require('path');
+const auth = require('http-auth');
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+const Registration = mongoose.model('Registration');
 const {check, validationResult} = require('express-validator');
+const Registration = require('../models/Registration');
 
 router.get('/', function(req, res) {
     res.render('form', { title: 'Registration form' });
 });
-
+router.get('/registrations', basic.check((req,res) => {
+  Registration.find()
+    .then((registrations) =>{
+        res.render('index', {title : 'Listing registrations', registrations });
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
+}));
 router.post('/', 
     [
     check('name')
@@ -19,7 +30,14 @@ router.post('/',
         // console.log(req.body);
         const errors = validationResult(req);
         if (errors.isEmpty()) {
-            res.send('Thank you for your registration!');
+          const registration = new Registration(req.body);
+          registration.save()
+            .then(() => { res.send('Thank you for your registration!'); })
+            .catch((err) => {
+                console.log(err);
+                res.send('Thank you for your registration!');
+            });
+            
         } else {
             res.render('form', {
                 title: 'Registration form',
